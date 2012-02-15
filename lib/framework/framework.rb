58920408f83c4f54f47cdfa530e226bf028fe994
Timothy Andrew <mail@timothyandrew.net>
2012-02-15 17:27:37 +0530

    Rename API class to Framework

--- lib/framework/framework.rb
+++ lib/framework/framework.rb
@@ -7,12 +7,19 @@ class Framework
   def route(uri, &function)
     @routes[uri] = function
   end
+  def default_route(&function)
+    @default_route = function
+  end
   def call(env)
     path = env["PATH_INFO"]
     begin
       @routes[path].call env
     rescue NoMethodError
-      [404, {'Content-Type' => 'text/html'}, ["<h1>Bad Route!</h1>"]]
+      begin
+        @default_route.call env
+      rescue NoMethodError
+        [404, { 'Content-Type' => 'text/html' }, 'Error in url!']
+      end
     end
   end
 end
