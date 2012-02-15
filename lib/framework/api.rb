7753f0043d3df2c3e379154c186e5b9f727cda15
Timothy Andrew <mail@timothyandrew.net>
2012-02-15 14:41:30 +0530

    basic framework functionality

---
+++ lib/framework/api.rb
@@ -0,0 +1,25 @@
+class API 
+  attr_reader :routes
+
+  def initialize
+    @routes = {}
+  end  
+  def route(uri, &function)
+    @routes[uri] = function
+  end
+  def default_route(&function)
+    @default_route = function
+  end
+  def call(env)
+    path = env["PATH_INFO"]
+    begin
+      @routes[path].call env
+    rescue NoMethodError
+      begin
+        @default_route.call env
+      rescue NoMethodError
+        [404, { 'Content-Type' => 'text/html' }, 'Error in url!']
+      end
+    end
+  end
+end
