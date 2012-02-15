bb31ea353100d284c7276037383a0af2d441155c
Timothy Andrew <mail@timothyandrew.net>
2012-02-15 17:23:44 +0530

    Removed 'default routing'. Unnecessary atm.

--- lib/framework/api.rb
+++ lib/framework/api.rb
@@ -7,19 +7,12 @@ class API
   def route(uri, &function)
     @routes[uri] = function
   end
-  def default_route(&function)
-    @default_route = function
-  end
   def call(env)
     path = env["PATH_INFO"]
     begin
       @routes[path].call env
     rescue NoMethodError
-      begin
-        @default_route.call env
-      rescue NoMethodError
-        [404, { 'Content-Type' => 'text/html' }, 'Error in url!']
-      end
+      [404, {'Content-Type' => 'text/html'}, ["<h1>Bad Route!</h1>"]]
     end
   end
 end
