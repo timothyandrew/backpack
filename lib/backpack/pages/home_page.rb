7bc8e0666f74fc7134839c51a2ece0ba60ed37f7
Timothy Andrew <mail@timothyandrew.net>
2012-02-16 18:09:52 +0530

    Changed to relative form post target.
    Thanks to Neil Jones for spotting this.

--- lib/backpack/pages/home_page.rb
+++ lib/backpack/pages/home_page.rb
@@ -2,10 +2,11 @@ require 'haml'
 require 'tilt'
 
 class HomePage
-  def initialize(env)    
+  def initialize(env)
+    @upload_link = "/api/upload"    
   end
   def get_response
     engine = Tilt.new('templates/home_page.haml')    
-    [200, { 'Content-Type' => 'text/html' }, [engine.render]]
+    [200, { 'Content-Type' => 'text/html' }, [engine.render(self)]]
   end
 end
