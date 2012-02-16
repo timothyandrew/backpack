109eb2155c904b027761642354c8a15bc353466e
Timothy Andrew <mail@timothyandrew.net>
2012-02-16 13:24:17 +0530

    Remove all but the bare dependencies.

--- lib/backpack.rb
+++ lib/backpack.rb
@@ -3,6 +3,3 @@ $LOAD_PATH.unshift(libdir) unless $LOAD_PATH.include?(libdir)
 
 require 'framework/framework'
 require 'backpack/app'
-require 'backpack/data_store'
-require 'backpack/file_viewer'
-require 'db/file_data'
--- lib/backpack/app.rb
+++ lib/backpack/app.rb
@@ -1,6 +1,5 @@
 class App < Framework
   def create_routes
-    route '/upload' , DataStore
-    route '/images/*', FileViewer
+    
   end
 end
