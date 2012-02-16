c14829280b722ae3233ec3a6b72762c266e98e0d
Timothy Andrew <mail@timothyandrew.net>
2012-02-16 15:46:20 +0530

    Spacing aesthetics.

--- lib/backpack/app.rb
+++ lib/backpack/app.rb
@@ -1,6 +1,6 @@
 class App < Framework
   def create_routes
-    route '/api/upload' , DataStore
-    route '/file/*' , FileViewer
+    route '/api/upload', DataStore
+    route     '/file/*', FileViewer
   end
 end
