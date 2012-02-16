d6ef448c4285c5a5f139ec837ff9ba54b8b5ca00
Timothy Andrew <mail@timothyandrew.net>
2012-02-16 12:34:15 +0530

    try a bogus require to see if heroku picks it up.

--- lib/backpack.rb
+++ lib/backpack.rb
@@ -3,6 +3,6 @@ $LOAD_PATH.unshift(libdir) unless $LOAD_PATH.include?(libdir)
 
 require 'framework/framework'
 require 'backpack/app'
-require 'backpack/data_store'
+require 'backpack/data_stor'
 require 'backpack/file_viewer'
 require 'db/file_data'
