37bcfb4136915f1d76e7871a08373bdc84dc3d21
Timothy Andrew <mail@timothyandrew.net>
2012-02-16 17:45:28 +0530

    Converted home page to haml.

--- lib/backpack/pages/home_page.rb
+++ lib/backpack/pages/home_page.rb
@@ -1,15 +1,11 @@
+require 'haml'
+require 'tilt'
+
 class HomePage
   def initialize(env)    
   end
   def get_response
-    [200, { 'Content-Type' => 'text/html' }, ['<form action="http://0.0.0.0:9393/api/upload" enctype="multipart/form-data" method="post">
-    <p>
-    Please specify a file, or a set of files:<br>
-    <input type="file" name="file" size="40">
-    </p>
-    <div>
-    <input type="submit" value="Send">
-    </div>
-    </form>']]
+    engine = Tilt.new('templates/home_page.haml')    
+    [200, { 'Content-Type' => 'text/html' }, [engine.render]]
   end
 end
