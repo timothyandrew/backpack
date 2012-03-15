d0c5acf5000340ffb49ee7eb5b94598616064425
Timothy Andrew <mail@timothyandrew.net>
2012-03-15 20:02:16 +0530

    Made username a primary key.

--- lib/db/dummy_data.rb
+++ lib/db/dummy_data.rb
@@ -1,14 +1,15 @@
 class DummyData
   def populate
-    25.times do
-      User.create(:username => ["john", "harry", "steve", "rachel", "sherlock", "frank"].sample + '_' + ["tucker", "holmes", "griffin", "simpson", "kennedy"].sample,
-                   :password => ["abcd123", "12345", "password", "guest"].sample)
+    10.times do |i|
+      User.create(:username => ["john", "harry", "steve", "rachel", "sherlock", "frank", "hannah", "peter", "victor", "jeremy"][i] + '_' + 
+                               ["tucker", "holmes", "griffin", "simpson", "kennedy", "clarkson"].sample,
+                  :password => ["abcd123", "12345", "password", "guest"].sample)
     end
     50.times do
       file = Filedatum.create(:md5sum => (0...64).map{ ('a'..'z').to_a[rand(26)] }.join,
                       :data   => (0...128).map{ ('a'..'z').to_a[rand(26)] }.join,
                       :mime_type => "text/html",
-                      :user => User.get((1..25).to_a.sample))      
+                      :user => User.get((1..10).to_a.sample))      
     end   
   end
 end
--- lib/db/user.rb
+++ lib/db/user.rb
@@ -3,9 +3,10 @@ require 'data_mapper'
 class User
   include DataMapper::Resource
   
-  property :id,       Serial
-  property :username, String
-  property :password, String             
+  property :username,      String, :key => true
+  property :password,      String             
+  property :auth_token,    String
+  property :token_expires, DateTime
   
   has n, :filedata
 end
