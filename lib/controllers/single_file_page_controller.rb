a1a853b0bbc9160607d644906b28c407f4a9807d
Timothy Andrew <mail@timothyandrew.net>
2012-04-26 23:46:14 +0530

    Add support for more file types. Video, PDF, Audio.

--- lib/controllers/single_file_page_controller.rb
+++ lib/controllers/single_file_page_controller.rb
@@ -2,5 +2,15 @@ class SingleFilePageController
   def initialize(env)
     hash = env['REQUEST_URI'].sub('/file/', '')
     @file_uri = "/api/file/#{hash}"
+    @mime_type = Filedatum.first(:md5sum => hash).mime_type
+
+    if @mime_type.split('/').first == 'video' || @mime_type.split('/').first == 'audio'
+      #Write video to file
+      file = DataRetrieve.new({ 'REQUEST_URI' => "/api/file/#{hash}" }).get_response.last
+      @file_path = File.join("static", "tmp", "#{hash}.#{@mime_type.split('/').last}")
+      f = File.new(File.join(Dir.pwd, @file_path), 'w+')
+        f.write(file.first)
+      f.close
+    end
   end
 end
